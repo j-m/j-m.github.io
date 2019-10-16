f5740eb8e30c59038e22cce3fa0c20d8b92f9ca5
Jonathan Marsh <hello@jonmarsh.dev>
2019-10-16 23:49:06 +0100

    WEB-48: Simple Sitemap of projects and Tags

--- src/routes/Sitemap.svelte
+++ src/routes/Sitemap.svelte
@@ -1,3 +1,46 @@
-<h1>
-Sitemap
-</h1>
+<script>
+import { onMount } from 'svelte'
+
+import Route from '../components/Route.svelte'
+
+import { getResources, getTags } from '../data/tagable.js'
+
+let projects = {}
+let tags = {}
+onMount(async () => {
+  projects = await getResources()
+  tags = await getTags()
+})
+</script>
+
+<style>
+.row {
+  display: flex;
+}
+
+.column {
+  flex: 1;
+}
+</style>
+
+
+<h1>Sitemap</h1>
+
+<div class="row">
+  <div class="column">
+    <h2>Tags</h2>
+    <ul>
+      {#each Object.entries(tags) as [id, data]}
+      <li><a href="/tag/{id}">{data.title}</a></li>
+      {/each}
+    </ul>
+  </div>
+  <div class="column">
+    <h2>Projects</h2>
+    <ul>
+      {#each Object.entries(projects) as [id, data]}
+      <li><a href="/project/{id}">{data.title}</a></li>
+      {/each}
+    </ul>
+  </div>
+</div>
