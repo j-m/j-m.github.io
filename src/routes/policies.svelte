fb8ac06af1a321e1d03d7a5e81ac42ac7e930017
Jonathan Marsh <hello@jonmarsh.dev>
2020-06-10 22:04:17 +0100

    WEB-80: Consistent header level. Added HRs

--- src/routes/policies.svelte
+++ src/routes/policies.svelte
@@ -3,11 +3,12 @@
 </svelte:head>
 
 <h1>Copyright</h1>
-<p>&copy; Copyright 2019, Jonathan Marsh.</p>
+<p>Website &copy; Copyright 2019, Jonathan Marsh.</p>
 <p>All rights reserved.</p>
 <p>See license infomation in each repository for exemptions and further information.</p>
+<hr/>
 
-<h1>Privacy/Cookie policy</h1>
+<h2>Privacy/Cookie policy</h2>
 
 <p>I do not directly use any cookies whatsoever, be it for tracking, functional purposes, or otherwise.</p>
 <p>Any cookies set or fetched on this website belong to 3rd party sources.</p>
@@ -18,13 +19,15 @@
 <p>As there are many web browsers out there, I cannot possibly give instructions for all of them.</p>
 <p>So, please refer to the manual/help section of your web browser or simply search online for how to disable cookies.</p>
 <p>Thank you.</p>
+<hr/>
 
-<h1>LinkedIn</h1>
+<h2>LinkedIn</h2>
 
 <p>LinkedIn, the LinkedIn logo, the IN logo and InMail are registered trademarks or trademarks of LinkedIn Corporation and its affiliates in the United States and/or other countries.</p>
 <p>LINKEDIN DISCLAIMS ANY WARRANTIES THAT MAY BE EXPRESS OR IMPLIED BY LAW REGARDING THE LINKEDIN MARKS (TO THE EXTENT PERMITTED BY LAW), INCLUDING WARRANTIES AGAINST INFRINGEMENT.</p>
 <p>You, the visitor, must not obtain the LinkedIn logo from jonmarsh.dev, please follow the <a href="https://brand.linkedin.com/policies">LinkedIn's branding policy</a> that is relevant for you.</p>
 
+<hr/>
 <h2>GitHub</h2>
 
 <p>GITHUB®, the GITHUB® logo design, OCTOCAT® and the OCTOCAT® logo design are exclusive trademarks registered in the United States by GitHub, Inc.<br/>
@@ -32,6 +35,7 @@ The OCTOCAT design is the exclusive property of GitHub, Inc and has been federal
 No adaptation or use of any kind of any of our registered trademarks or copyrights, or any other contents of this website, is allowed without the express written permission of GitHub, Inc.</p>
 <p>You, the visitor, must not obtain the GitHub logo from jonmarsh.dev, please refer to <a href="https://github.com/logos">GitHub's branding policies</a>.</p>
 
+<hr/>
 <h2>StackOverflow</h2>
 <p>The Stack Exchange name and logos are trademarks of Stack Exchange Inc. The names and logos for sites and products operating on the Stack Exchange network are also trademarks of Stack Exchange Inc.</p>
 <p>You, the visitor, must not obtain the StackOverflow logo from jonmarsh.dev, please refer to <a href="https://stackoverflow.com/legal/trademark-guidance">StackOverflow's branding policies</a> which permits use of <a href="https://stackoverflow.com/company/logos">StackOverflow's logos</a>.</p>
