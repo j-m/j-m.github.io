2f50f581538f69dd52cef67ace823f583b5767c7
Jonathan Marsh <hello@jonmarsh.dev>
2020-06-15 12:12:35 +0100

    WEB-85: Sitemap mobile view

--- src/routes/sitemap.svelte
+++ src/routes/sitemap.svelte
@@ -34,10 +34,12 @@ for (let [key, project] of Object.entries(tagable.resources)) {
 <style>
 .row {
   display: flex;
+  flex-wrap: wrap;
 }
 
 .column {
   flex: 1;
+  min-width: 15rem;
 }
 .column ul li a {
   text-decoration: none!important;
